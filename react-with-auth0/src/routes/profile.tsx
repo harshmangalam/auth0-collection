0d66c3cfbf6714d3b721cad2a3815137b7b5b6c1
harshmangalam <harshdev8218@gmail.com>
2022-10-17 14:25:04 +0530

    updated profile

--- react-with-auth0/src/routes/profile.tsx
+++ react-with-auth0/src/routes/profile.tsx
@@ -7,38 +7,28 @@ export default function Profile() {
     return <p>Loading...</p>;
   }
   return (
-    <div className="flex items-center flex-col space-y-4">
+    <div className="flex items-center justify-center flex-col space-y-4">
       <img src={user?.picture} alt={user?.name} className="w-56 h-56 rounded" />
-      <div>
-        <h1>{user?.name}</h1>
-        <h2>{user?.email}</h2>
+      <div className="flex flex-col items-center">
+        <h1 className="text-3xl">{user?.name}</h1>
+        <h2 className="text-2xl">{user?.email}</h2>
 
-        <h3>
-          Account updated: {new Date(user?.updated_at).toLocaleDateString()}
-        </h3>
-        <h3>Locale: {user?.locale}</h3>
+        {user?.updated_at && (
+          <h3 className="text-xl">
+            Account updated: {new Date(user?.updated_at).toLocaleDateString()}
+          </h3>
+        )}
+        {user?.locale && <h3 className="text-xl">Locale: {user?.locale}</h3>}
       </div>
       <div
         className={`px-4 border py-1 text-sm text-white inline-flex rounded-full font-semibold ${
-          user?.email_verified ? "border-green-500 text-green-500" : "border-red-500 text-red-500"
+          user?.email_verified
+            ? "border-green-500 text-green-500"
+            : "border-red-500 text-red-500"
         }`}
       >
         {user?.email_verified ? "Verified" : "Not Verified"}
       </div>
-      
     </div>
   );
 }
-
-// {
-//     "given_name": "Harsh",
-//     "family_name": "Mangalam",
-//     "nickname": "harshdev8218",
-//     "name": "Harsh Mangalam",
-//     "picture": "https://lh3.googleusercontent.com/a/ALm5wu1YSkuVNyHm_v3FG3tmMjUWBzK8V3a3nOwV-CAU=s96-c",
-//     "locale": "en",
-//     "updated_at": "2022-10-17T08:34:49.150Z",
-//     "email": "harshdev8218@gmail.com",
-//     "email_verified": true,
-//     "sub": "google-oauth2|111627030256528312080"
-// }
