753c82e09a0fc7555fb76b5abcd94ca91516035a
harshmangalam <harshdev8218@gmail.com>
2022-10-17 13:54:38 +0530

    added home path

--- react-with-auth0/src/router.tsx
+++ react-with-auth0/src/router.tsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "./routes/home";
 import Root from "./routes/root";
 
 export default createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
