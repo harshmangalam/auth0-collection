5ecea465ad9841f9f71ea2d6cfe18d1e507567fa
harshmangalam <harshdev8218@gmail.com>
2022-10-17 14:08:29 +0530

    added auth0 provider

--- react-with-auth0/src/main.tsx
+++ react-with-auth0/src/main.tsx
@@ -1,11 +1,18 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {  RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import router from "./router"
+import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Auth0Provider
+      clientId={import.meta.env.VITE_CLIENT_ID}
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      redirectUri={window.location.origin}
+    >
+      <RouterProvider router={router} />
+    </Auth0Provider>
   </React.StrictMode>
 );
