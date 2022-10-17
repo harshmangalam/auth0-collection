671a286b641ebf2f529c6f184da9bb8a0360e7dd
harshmangalam <harshdev8218@gmail.com>
2022-10-17 14:26:08 +0530

    added home page link

--- react-with-auth0/src/components/navbar/index.tsx
+++ react-with-auth0/src/components/navbar/index.tsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav className="w-full border-b bg-white">
-      <div className="h-16 flex items-center container mx-auto px-6 justify-between">
+      <Link
+        to={"/"}
+        className="h-16 flex items-center container mx-auto px-6 justify-between"
+      >
         <img className="w-12 h-12" src="auth0.svg" alt="Auth0" />
-      </div>
+      </Link>
     </nav>
   );
 }
