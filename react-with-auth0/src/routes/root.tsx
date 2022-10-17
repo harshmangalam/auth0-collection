7798637dc934261e6f8a5bebef43bc6bbdb3586c
harshmangalam <harshdev8218@gmail.com>
2022-10-17 13:55:09 +0530

    added root layout

--- react-with-auth0/src/routes/root.tsx
+++ react-with-auth0/src/routes/root.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/navbar";
 
 export default function Root() {
   return (
-    <div>
-      <Outlet />
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
